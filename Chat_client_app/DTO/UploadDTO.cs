89afe407ad2cbb6fea09b7ecbe734851f9764f4d
Yaroslavhal <yaroslav.halushko.n@gmail.com>
2022-12-26 06:22:09 +0200

    Added commit  with explanations

---
+++ Chat_client_app/DTO/UploadDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chat_client_app.DTO
+{
+    public class UploadDTO
+    {
+        public string Photo { get; set; }
+    }
+}
